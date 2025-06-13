76c93923c953ce80a2093550f2be77a642039aaf
GitGuru2003 <email2munib@gmail.com>
2025-06-13 01:28:21 -0600

    More Api endpoints

---
+++ Dtos/UserToUpdateDto.cs
@@ -0,0 +1,10 @@
+namespace chatbot.Dtos
+{
+  public partial class UserToUpdateDto
+  {
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Gender { get; set; } = "";
+  }
+}
